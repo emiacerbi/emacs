8aa97675211ca678045447bc084420e49ed014be
Emiliano Acerbi <eacerbip@gmail.com>
2024-08-01 14:48:15 -0300

    more tools

--- init.el
+++ init.el
@@ -1,4 +1,3 @@
-;;disable splash screen and startup message
 (setq inhibit-startup-message t) 
 (setq initial-scratch-message nil)
 ;;(menu-bar-mode -1)
@@ -23,8 +22,8 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/")) 
 (package-initialize)
 (unless (package-installed-p 'use-package) 
-(package-refresh-contents)
- (package-install 'use-package)) 
+  (package-refresh-contents)
+  (package-install 'use-package)) 
 (eval-and-compile 
   (setq use-package-always-ensure t
 	use-package-expand-minimally t))
@@ -45,7 +44,45 @@
 (setq ido-everywhere t)
 (ido-mode 1)
 
+(use-package lsp-mode
+  :ensure t
+  :commands (lsp lsp-deferred)
+  :hook ((js-mode . lsp-deferred)
+         (typescript-mode . lsp-deferred)
+         (web-mode . lsp-deferred))
+  :config
+  (setq lsp-eslint-auto-fix-on-save t))
 
-(setq lsp-eslint-auto-fix-on-save t)
+;; Web Mode for JSX/TSX files
+(use-package web-mode
+  :ensure t
+  :mode (("\\.js\\'" . web-mode)
+         ("\\.jsx\\'" . web-mode)
+         ("\\.ts\\'" . web-mode)
+         ("\\.tsx\\'" . web-mode))
+  :config
+  (setq web-mode-enable-auto-quoting nil))  ;; Disable automatic insertion of quotes
 
+;; Ensure the use of eslint with flycheck
+(use-package flycheck
+  :ensure t
+  :init
+  (global-flycheck-mode)
+  :config
+  (flycheck-add-mode 'javascript-eslint 'web-mode))
 
+;; Typescript-mode
+(use-package typescript-mode
+  :ensure t
+  :mode "\\.ts\\'"
+  :hook (typescript-mode . lsp-deferred))
+
+;; Adding json-mode for JSON files
+(use-package json-mode
+  :ensure t
+  :mode "\\.json\\'")
+
+;; Adding yaml-mode for YAML files
+(use-package yaml-mode
+  :ensure t
+  :mode "\\.ya?ml\\'")
